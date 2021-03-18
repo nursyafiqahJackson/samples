8f547b8da84d7536068d610b8de34263153c14a6
Nursyafiqah Jackson <user@DESKTOP-NV76QCT>
2021-03-18 22:36:14 +0800

    Commit

--- samples/src/samples/demo/GitTest.java
+++ samples/src/samples/demo/GitTest.java
@@ -2,9 +2,19 @@ package samples.demo;
 
 public class GitTest {
 
+	private class Location {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is fun");
+		System.out.println("Git is cool");
 	}
 
 }
